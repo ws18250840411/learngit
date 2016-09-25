8fe7e9320f8bf69e08404562efc114ccce802b4a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2016-09-25 18:27:15 +0800

    合并分支

--- apple.js
+++ apple.js
@@ -4,4 +4,4 @@ console.log("我又新加了内容咯1111111111");
 console.log("我又新加了内容咯2222222222");
 console.log("我又新加了内容咯3333333333");
 console.log("我又新加了内容咯4444444444");
-console.log("Creating a new branch is quick and simple.")
+console.log("我又创建了一个新的分支vincent");
